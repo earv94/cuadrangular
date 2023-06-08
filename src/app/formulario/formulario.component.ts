898423af3e963e6450bb092b4308800ee4c03166
earv94 <earv94@gmail.com>
2023-06-07 20:17:32 -0500

    refactorizacion de seccion de calculo en caso de empate

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -16,62 +16,54 @@ export class FormularioComponent {
   public corrimiento = 0;
   show:boolean=false;
 
+  verificarEmpate(nombre: string){
+    //si el nombre esta en la tabla suma el punto en la misma posicion del equipo en el array tabla
+    if((this.tabla.includes(nombre)) && (nombre != null)) {
+      this.puntuaciones[this.tabla.indexOf(nombre)]++; 
+    }
+    //si nombre no esta en la tabla y nombre no es nulo a;ade el nombre en la posicion del corrimiento, mueve el corrimiento y 
+    //suma el punto en la misma posicion del equipo en el array tabla
+    else { 
+      this.tabla[this.corrimiento] = nombre;
+      this.corrimiento++;
+      this.puntuaciones[this.tabla.indexOf(nombre)]++; 
+    }
+  }
+
   calculo(nombre1: string, nombre2: string, resultado1: number, resultado2: number) {
     console.log(this.puntuaciones[0]);
       //validacion equipo a gana
     if (resultado1 > resultado2) {
       let flag = 5;
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < 3; i++) { //verificar que este en la tabla
         if (this.tabla[i] === nombre1 && nombre1 != null) {
-          flag = i;
+          flag = i;//guarda la ubicacion del equipo
         }
       }
-      if (flag == 5) {
-        this.tabla[this.corrimiento] = nombre1;
-        this.puntuaciones[this.corrimiento]=0;
-        flag=this.corrimiento;
+      if (flag == 5) {//entra cuando no esta en tabla
+        this.tabla[this.corrimiento] = nombre1; //a;ade al equipo en la posicion del corrimiento
+        flag=this.corrimiento; //guarda la ubicacion del equipo y desplaza el corrimiento
         this.corrimiento++;
       }
-      this.puntuaciones[flag] = this.puntuaciones[flag] + 3;
+      this.puntuaciones[flag] = this.puntuaciones[flag] + 3; //suma +3 al equipo ganador
       //validacion equipo b gana
     } else if (resultado1 < resultado2) {
       let flag = 5;
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < 3; i++) { //verificar que este en la tabla
         if (this.tabla[i] === nombre2 && nombre2 != null) {
-          flag = i;
+          flag = i;//guarda la ubicacion del equipo
         }
       }
-      if (flag == 5) {
-        this.tabla[this.corrimiento] = nombre2;
-        this.puntuaciones[this.corrimiento]=0;
-        flag=this.corrimiento;
+      if (flag == 5) {//entra cuando no esta en tabla
+        this.tabla[this.corrimiento] = nombre2;//a;ade al equipo en la posicion del corrimiento
+        flag=this.corrimiento; //guarda la ubicacion del equipo y desplaza el corrimiento
         this.corrimiento++;
       }
       this.puntuaciones[flag] += 3;
       //validacion empate
     } else {
-      //si el nombre esta en la tabla suma el punto en la misma posicion del equipo en el array tabla
-      if((this.tabla.includes(nombre1)) && (nombre1 != null)) {
-        this.puntuaciones[this.tabla.indexOf(nombre1)]++; 
-      }
-      //si nombre no esta en la tabla y nombre no es nulo a;ade el nombre en la posicion del corrimiento, mueve el corrimiento y 
-      //suma el punto en la misma posicion del equipo en el array tabla
-      else { 
-        this.tabla[this.corrimiento] = nombre1;
-        this.corrimiento++;
-        this.puntuaciones[this.tabla.indexOf(nombre1)]++; 
-      }
-      //si el nombre esta en la tabla suma el punto en la misma posicion del equipo en el array tabla
-      if((this.tabla.includes(nombre2)) && (nombre2 != null)) {
-        this.puntuaciones[this.tabla.indexOf(nombre2)]++; 
-      }
-      //si nombre no esta en la tabla y nombre no es nulo a;ade el nombre en la posicion del corrimiento, mueve el corrimiento y 
-      //suma el punto en la misma posicion del equipo en el array tabla
-      else { 
-        this.tabla[this.corrimiento] = nombre2;
-        this.corrimiento++;
-        this.puntuaciones[this.tabla.indexOf(nombre2)]++;
-      }
+      this.verificarEmpate(nombre1);
+      this.verificarEmpate(nombre2);
     }
   }
 
@@ -90,12 +82,6 @@ export class FormularioComponent {
       console.log("Equipo:    ",this.tabla[i],"  Resultado:    ", this.puntuaciones[i]);
     }
 
-    this.show=true;
-
-    /*
-    si el corrimiento no termina en 4 es porque no lleno con todos los equipos
-    if(this.corrimiento!=4){
-      for ()
-    }*/
+    this.show=true; //para mostrar los resultados ya que al inicio estan ocultos
   }
 }
